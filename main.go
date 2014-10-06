f1277fe63ed8365fc0dac53902f14ec47ab7c843
Emanuel Andersson <manusdude@gmail.com>
2014-10-06 23:12:18 +0200

    Stdin with channels.

--- main.go
+++ main.go
@@ -9,39 +9,6 @@ import (
 	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	conn, err := websocket.Upgrade(w, r, w.Header(), 4096, 4096)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	log.Printf("Incoming connection: %s\n", r.RemoteAddr)
-	defer log.Printf("Connection closed: %s\n", r.RemoteAddr)
-
-	inReader := bufio.NewReader(os.Stdin)
-	log.Printf("Stream started: STDIN -> %v\n", r.RemoteAddr)
-	first := true
-
-	for {
-		line, err := inReader.ReadBytes('\n')
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		if first {
-			log.Println(string(line))
-			first = false
-		}
-
-		if err = conn.WriteMessage(websocket.TextMessage, line); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
 func main() {
 
 	if len(os.Args) != 2 {
@@ -52,8 +19,75 @@ func main() {
 	port := fmt.Sprintf(":%s", os.Args[1])
 	log.Printf("Starting WebSocket server on port %v\n", port)
 
-	http.HandleFunc("/", handler)
+	subscribers := make(chan chan []byte)
+	go readStdin(subscribers)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r, subscribers)
+	})
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatalln(err)
+	}
+}
+
+func readStdin(subscribers chan chan []byte) {
+	allSubscribers := make([]chan []byte, 0)
+
+	stdin := make(chan []byte)
+	go func(stdin chan []byte) {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			line, _, err := reader.ReadLine()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			stdin <- line
+		}
+	}(stdin)
+
+	for {
+		select {
+		case subscription := <-subscribers:
+			allSubscribers = append(allSubscribers, subscription)
+		case line := <-stdin:
+			activeSubscribers := make([]chan []byte, 0)
+			for i := range allSubscribers {
+				_, ok := <-allSubscribers[i]
+				if !ok {
+					continue
+				} else {
+					allSubscribers[i] <- line
+					activeSubscribers = append(activeSubscribers, allSubscribers[i])
+				}
+			}
+			allSubscribers = activeSubscribers
+		}
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request, subscribers chan chan []byte) {
+	conn, err := websocket.Upgrade(w, r, w.Header(), 4096, 4096)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Printf("Incoming connection: %s\n", r.RemoteAddr)
+	subscription := make(chan []byte)
+	subscribers <- subscription
+	subscription <- []byte("acc")
+	defer close(subscription)
+	defer log.Printf("Connection closed: %s\n", r.RemoteAddr)
+
+	log.Printf("Stream started: STDIN -> %v\n", r.RemoteAddr)
+
+	for {
+		line := <-subscription
+		if err = conn.WriteMessage(websocket.TextMessage, line); err != nil {
+			log.Println(err)
+			return
+		}
+		subscription <- []byte("acc")
 	}
 }
